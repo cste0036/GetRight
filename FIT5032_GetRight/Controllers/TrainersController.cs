5c260ace40671ac4e1ff7f694ed6f44387a19cce
cste0036 <cste0036@student.monash.edu>
2022-09-18 12:08:25 +1000

    Added User Roles

---
+++ FIT5032_GetRight/Controllers/TrainersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIT5032_GetRight.Models;
+
+namespace FIT5032_GetRight.Controllers
+{
+    public class TrainersController : Controller
+    {
+        private FIT5032_GetRightModel db = new FIT5032_GetRightModel();
+
+        // GET: Trainers
+        public ActionResult Index()
+        {
+            var trainers = db.Trainers.Include(t => t.Gym);
+            return View(trainers.ToList());
+        }
+
+        // GET: Trainers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // GET: Trainers/Create
+        public ActionResult Create()
+        {
+            ViewBag.GymId = new SelectList(db.Gyms, "GymId", "GymName");
+            return View();
+        }
+
+        // POST: Trainers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TrainerId,FirstName,LastName,UserId,Tags,Description,GymId")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trainers.Add(trainer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GymId = new SelectList(db.Gyms, "GymId", "GymName", trainer.GymId);
+            return View(trainer);
+        }
+
+        // GET: Trainers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GymId = new SelectList(db.Gyms, "GymId", "GymName", trainer.GymId);
+            return View(trainer);
+        }
+
+        // POST: Trainers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TrainerId,FirstName,LastName,UserId,Tags,Description,GymId")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trainer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GymId = new SelectList(db.Gyms, "GymId", "GymName", trainer.GymId);
+            return View(trainer);
+        }
+
+        // GET: Trainers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // POST: Trainers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trainer trainer = db.Trainers.Find(id);
+            db.Trainers.Remove(trainer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
